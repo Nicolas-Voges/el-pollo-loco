ae1ba1ae826d01c9cae8d78dc97e7869c5d884e4
Nicolas Voges <nicolas.voges@hotmail.com>
2024-06-16 01:39:05 +0200

    Add documentation to bottle.class.js

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -5,6 +5,22 @@ class Bottle extends CollectableObject {
     offsetLeft = 22;
     width = 60;
     height = 60;
+    IMAGES = [
+        'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
+        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png',
+        'img/6_salsa_bottle/salsa_bottle.png'
+    ];
+
+    /**
+     * This first called function calls the constructor function of CollectableObject class and hands over its given
+     * x, y and imgIndex variables.
+     * Than it loads the objects image and the IMAGES Array.
+     * After that it calls the chooseOffset function and hands over the imgIndex.
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} imgIndex 
+     */
     constructor(x, y, imgIndex) {
         super(x, y).loadImage(this.IMAGES[imgIndex]);
         this.loadImages(this.IMAGES);
@@ -13,12 +29,11 @@ class Bottle extends CollectableObject {
         // this.animate();
     }
 
-    IMAGES = [
-        'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
-        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png',
-        'img/6_salsa_bottle/salsa_bottle.png'
-    ];
-
+    /**
+     * This function sets the offset values based on the images index.
+     * 
+     * @param {number} imgIndex 
+     */
     chooseOffset(imgIndex) {
         switch (imgIndex) {
             case 0:
@@ -39,12 +54,15 @@ class Bottle extends CollectableObject {
                 this.offsetBottom = 8;
                 this.offsetLeft = 23;
                 break;
-
             default:
                 break;
         }
     }
 
+    /**
+     * This function animates this object by changing its image index by calling playAnimation function
+     * in an interval of 2Hz and registers this interval.
+     */
     animate() {
         let intervalBottleAnimate = setInterval(() => {
             this.playAnimation(this.IMAGES);
